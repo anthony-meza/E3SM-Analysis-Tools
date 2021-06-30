20475c673f073eed775db7c94ffc69fc82779b85
anthony-meza <64243783+anthony-meza@users.noreply.github.com>
2021-06-30 14:33:45 -0700

    Update apply_patches.sh

--- Single Precision Patches/apply_patches.sh
+++ Single Precision Patches/apply_patches.sh
@@ -2,13 +2,17 @@
 
 #single_precision_mpas.patch: The paths in this file assume you are in the E3SM root directory.
 patch single_precision_mpas.patch
+cp single_precision_bgc.patch ./components/mpas-ocean/src/BGC/single_precision_bgc.patch
+cp single_precision_cvmix.patch ./components/mpas-ocean/src/cvmix/single_precision_cvmix.patch
+cp single_precision_gotm.patch ./components/mpas-ocean/src/gotm/single_precision_gotm.patch
+
 #single_precision_bgc.patch: The paths in this file assume you are in E3SM/components/mpas-ocean/src/BGC
-cd /components/mpas-ocean/src/BGC
-patch single_precision_bgc.patch
+cd ./components/mpas-ocean/src/BGC
+git apply single_precision_bgc.patch
 #single_precision_cvmix.patch: The paths in this file assume you are in E3SM/components/mpas-ocean/src/cvmix
 cd ../cvmix
-patch single_precision_cvmix.patch
+git apply single_precision_cvmix.patch
 
 #single_precision_gotm.patch: The paths in this file assume you are in E3SM/components/mpas-ocean/src/gotm
 cd ../gotm 
-patch single_precision_gotm.patch
+git apply single_precision_gotm.patch
